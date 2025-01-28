bcebd9208aca339429aefe9441076ef577beed65
BhimaRakesh <brakeshrakesh5@gmail.com>
2025-01-29 03:02:00 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
 import React,{useState} from 'react'
 import "./App.css"
 const App = () => {
-
-  const [city ,setCity]=useState("")
-const [result ,setResult]=useState("")
-    const changeHandler =(e)=>{
-setCity (e.target.value)
+  const [city ,setCity]=useState(" ")
+  const [resultInCelcius ,setResultInCelcius]=useState(" ")
+const [resultinKelvin ,setResultInKelvin] =useState("")
+  const changeHandler =(e)=>{
+setCity(e.target.value)
   }
-
-  const submitHandler = (e)=>{
-   e.preventDefault()
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then( 
-response => response.json()).then(data => {
-  const kelvin = data.main.temp
-  const celcius = kelvin - 273;
-  console.log(kelvin)
-  console.log(celcius)
-  setResult("Temperature at "+city+Math.round(celcius)+"°C")
-}
-
-)
+  const celciusHandler =(e)=>{
+e.preventDefault()
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then(
+  response => response.json()).then(data=>{
+    const kelvin = data.main.temp
+    const celcius = kelvin - 273.15 ;
+    console.log(kelvin)
+    setResultInCelcius("Temapareture in "+city+" "+Math.round(celcius)+" °C")
   }
+  ).catch(error => console.log(error))
+  setCity("");
 
+  }
+  const kelvinHandler =(e)=>{
+    e.preventDefault()
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then(
+      response =>response.json()).then(
+        data => {
+        const kelvin =data.main.temp
+        setResultInKelvin("Temapareture in " +city+" "+Math.round(kelvin)+" k")
+      }).catch(error =>console.log(error))
+      setCity("")
+    
+  }
   return (
     <div className='card'>
       <center>
-      <h1>Weather reporter</h1>
-      {result}
-      <form onSubmit={submitHandler}>
-      <input type ="text" name="city" onChange={changeHandler} value = {city} placeholder='enter city'/>
-      <input type ="submit" value="get tempareture"/>
-      </form>
+      
+      <h2>weather-Reporter</h2>
+      <input type="text" onChange={changeHandler} value={city} name="city" /><br/>
+      <button onClick={celciusHandler}>temp get in celcius </button><br/>
+      <button onClick={kelvinHandler}>tempareture in kelvin</button>
+
+
+
+
 </center>
+{resultInCelcius}<br></br>
+{resultinKelvin}
     </div>
-    
   )
 }
 
